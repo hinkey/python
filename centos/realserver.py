a9165e024f24326b0ec1eb0b2978fdaa1cbd56f4
hinkey <ad8jk@163.com>
2015-12-17 19:07:09 +0800

    改进，加了时间

--- centos/realserver.py
+++ centos/realserver.py
@@ -5,33 +5,43 @@ import threading
 import time
 from __builtin__ import str
 import os
+import logging
+
+LOG_FILENAME="log.txt"
+logging.basicConfig(filename=LOG_FILENAME,level=logging.NOTSET)
 
 def httpStatus():     
     try:        
         page=urllib2.urlopen('http://localhost:3000/api/err',timeout=2)
     except Exception,e:
             print "http err is "+str(e)
-            print '服务错误，第一次检测，重新启动pm2' 
+            print '服务错误，第一次检测，重新启动pm2,错误时间是：' +time.strftime('%Y-%m-%d %H:%M:%S')
             temp = os.popen('pm2 start /Users/hinkey/Documents/working/nodejs/mobileCloud/src/server.js')  
-            print temp.read()            
+            print temp.read()
+            logging.debug("http err is "+str(e))
+            logging.debug('服务错误，第一次检测，重新启动pm2,错误时间是：' +time.strftime('%Y-%m-%d %H:%M:%S'))
         
     time.sleep(3)
     try:        
         page=urllib2.urlopen('http://localhost:3000/api/err',timeout=2)
     except Exception,e:
-            print "err is "+str(e)
-            print '服务错误，第一次检测，重新启动pm2' 
+            print "http err is "+str(e)
+            print '服务错误，第二次检测，重新启动pm2,错误时间是：' +time.strftime('%Y-%m-%d %H:%M:%S')
             temp = os.popen('pm2 start /Users/hinkey/Documents/working/nodejs/mobileCloud/src/server.js')  
-            print temp.read()  
+            print temp.read()
+            logging.debug("http err is "+str(e))
+            logging.debug('服务错误，第二次检测，重新启动pm2,错误时间是：' +time.strftime('%Y-%m-%d %H:%M:%S'))
           
     time.sleep(3)
     try:        
         page=urllib2.urlopen('http://localhost:3000/api/err',timeout=2)
     except Exception,e:
-            print "err is "+str(e)
-            print '服务错误，第一次检测，重新启动pm2' 
+            print "http err is "+str(e)
+            print '服务错误，第三次检测，重新启动pm2,错误时间是：' +time.strftime('%Y-%m-%d %H:%M:%S')
             temp = os.popen('pm2 start /Users/hinkey/Documents/working/nodejs/mobileCloud/src/server.js')  
-            print temp.read()  
+            print temp.read()
+            logging.debug("http err is "+str(e))
+            logging.debug('服务错误，第三次检测，重新启动pm2,错误时间是：' +time.strftime('%Y-%m-%d %H:%M:%S'))
 
 #timer定时器
 def checkHttp():
